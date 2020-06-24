0bfab60b23ca305d8cf05aa4dff0e43d0c6ca7bd
asagafonov <a.s.agafonov@yandex.ru>
2020-06-24 23:18:23 +0300

    upd output formats' description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ const genDiff = program
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'choose output format', 'stylish')
+  .option('-f, --format [type]', 'choose output format: stylish | plain | json', 'stylish')
   .action((filepath1, filepath2) => compareFiles(filepath1, filepath2, program.format));
 
 program.parse(process.argv);
