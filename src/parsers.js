f7b5e4d34d8e59f9ef762796e5d044c529ab8a06
asagafonov <a.s.agafonov@yandex.ru>
2020-08-18 17:41:01 +0300

    upd names

--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,33 @@
+import _ from 'lodash';
 import YAML from 'js-yaml';
 import INI from 'ini';
-import { fixIniParser } from './utils.js';
 
-const parseFile = (fileContents, extension) => {
+const digitsOnly = (string) => [...string].every((letter) => '0123456789'.includes(letter));
+
+const fixIniParser = (obj) => {
+  const entries = Object.entries(obj);
+  return entries.reduce((acc, element) => {
+    const [key, value] = element;
+    if (_.isPlainObject(value)) {
+      return { ...acc, [key]: fixIniParser(value) };
+    }
+    if (typeof value === 'string') {
+      if (digitsOnly(value)) {
+        return { ...acc, [key]: Number(value) };
+      }
+    }
+    return { ...acc, [key]: value };
+  }, {});
+};
+
+const parseFile = (fileContent, extension) => {
   switch (extension) {
     case '.yml':
-      return YAML.safeLoad(fileContents);
+      return YAML.safeLoad(fileContent);
     case '.ini':
-      return fixIniParser(INI.parse(fileContents));
+      return fixIniParser(INI.parse(fileContent));
     case '.json':
-      return JSON.parse(fileContents);
+      return JSON.parse(fileContent);
     default:
       throw new Error(`Unknown file extension: ${extension}`);
   }
