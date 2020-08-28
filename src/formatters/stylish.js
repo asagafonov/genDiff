d4fe4001a30c31c0018ccda397fa4f84eb29e8dc
asagafonov <a.s.agafonov@yandex.ru>
2020-08-28 23:54:49 +0300

    upd stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,3 +1,18 @@
+import _ from 'lodash';
+
+const expand = (val) => {
+  if (!_.isObject(val)) {
+    return val;
+  }
+  const keys = _.keys(val);
+  return keys.map((key) => {
+    if (!_.isObject(val[key])) {
+      return { name: key, status: 'unknown', value: val[key] };
+    }
+    return { name: key, status: 'unknown', value: expand(val[key]) };
+  });
+};
+
 const generateStylishDiff = (diff) => {
   const iter = (data, depth1, depth2) => data
     .map((item) => {
@@ -8,17 +23,21 @@ const generateStylishDiff = (diff) => {
       const indent1 = space.repeat(depth1);
       const indent2 = space.repeat(depth2);
 
-      const chooseValType = (m) => (Array.isArray(m) ? `{\n${iter(m, depth1 + 4, depth2 + 4).join('\n')}\n${indent1}}` : m);
+      const chooseIndent = (m) => (Array.isArray(m) ? `{\n${iter(m, depth1 + 4, depth2 + 4).join('\n')}\n${indent1}}` : m);
+
+      const formatValue = (val) => chooseIndent(expand(val));
 
       switch (status) {
-        case 'unmodified':
-          return [`${indent1}${name}: ${chooseValType(value)}`];
-        case 'modified':
-          return [`${indent2}- ${name}: ${chooseValType(oldValue)}\n${indent2}+ ${name}: ${chooseValType(newValue)}`];
         case 'added':
-          return [`${indent2}+ ${name}: ${chooseValType(value)}`];
+          return [`${indent2}+ ${name}: ${formatValue(value)}`];
         case 'deleted':
-          return [`${indent2}- ${name}: ${chooseValType(value)}`];
+          return [`${indent2}- ${name}: ${formatValue(value)}`];
+        case 'unknown':
+          return [`${indent1}${name}: ${chooseIndent(value)}`];
+        case 'unmodified':
+          return [`${indent1}${name}: ${formatValue(value)}`];
+        case 'modified':
+          return [`${indent2}- ${name}: ${formatValue(oldValue)}\n${indent2}+ ${name}: ${formatValue(newValue)}`];
         default:
           throw new Error(`Unknown status ${status}`);
       }
