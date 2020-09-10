c96933ee693c8ab19d1c484b876c05407b2690ea
asagafonov <a.s.agafonov@yandex.ru>
2020-09-10 12:55:05 +0300

    fix __dirname issue

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@jest/globals';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const __dirname = path.resolve();
-const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', `/${filename}`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', `/${filename}`);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [
