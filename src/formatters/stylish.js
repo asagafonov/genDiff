cdf0202e9587d955b188063595a87ebb018d9117
asagafonov <a.s.agafonov@yandex.ru>
2020-09-10 14:23:07 +0300

    fix indents

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,45 +7,45 @@ const expand = (val) => {
   const keys = _.keys(val);
   return keys.map((key) => {
     if (!_.isObject(val[key])) {
-      return { name: key, status: 'unknown', value: val[key] };
+      return { name: key, status: 'unknown', children: val[key] };
     }
-    return { name: key, status: 'unknown', value: expand(val[key]) };
+    return { name: key, status: 'unknown', children: expand(val[key]) };
   });
 };
 
-const formatDiffAsStylish = (diff) => {
-  const iter = (data, depth1, depth2) => data
-    .map((item) => {
+export default (diff) => {
+  const iter = (data, depth) => data
+    .flatMap((item) => {
       const {
-        name, status, value, oldValue, newValue,
+        name, status, value, children, oldValue, newValue,
       } = item;
       const space = ' ';
-      const indent1 = space.repeat(depth1);
-      const indent2 = space.repeat(depth2);
+      const initialIndent1 = space.repeat(4);
+      const initialIndent2 = space.repeat(2);
+      const indent1 = initialIndent1 + space.repeat(depth * 4);
+      const indent2 = initialIndent2 + space.repeat(depth * 4);
 
-      const chooseIndent = (m) => (Array.isArray(m) ? `{\n${iter(m, depth1 + 4, depth2 + 4).join('\n')}\n${indent1}}` : m);
+      const chooseIndent = (m) => (Array.isArray(m) ? `{\n${iter(m, depth + 1).join('\n')}\n${indent1}}` : m);
 
       const formatValue = (val) => chooseIndent(expand(val));
 
       switch (status) {
         case 'added':
-          return [`${indent2}+ ${name}: ${formatValue(value)}`];
+          return `${indent2}+ ${name}: ${formatValue(value)}`;
         case 'deleted':
-          return [`${indent2}- ${name}: ${formatValue(value)}`];
+          return `${indent2}- ${name}: ${formatValue(value)}`;
         case 'unknown':
-          return [`${indent1}${name}: ${chooseIndent(value)}`];
+          return `${indent1}${name}: ${chooseIndent(children)}`;
         case 'unmodified':
-          return [`${indent1}${name}: ${formatValue(value)}`];
+          return `${indent1}${name}: ${formatValue(value)}`;
         case 'modified':
-          return [`${indent2}- ${name}: ${formatValue(oldValue)}\n${indent2}+ ${name}: ${formatValue(newValue)}`];
+          return [
+            [`${indent2}- ${name}: ${formatValue(oldValue)}`],
+            [`${indent2}+ ${name}: ${formatValue(newValue)}`]
+          ];
         default:
           throw new Error(`Unknown status ${status}`);
       }
     });
-  return iter(diff, 4, 2);
-};
-
-export default (diff) => {
-  const stylishDiff = formatDiffAsStylish(diff);
-  return `{\n${stylishDiff.join('\n')}\n}`;
+  return `{\n${iter(diff, 0).join('\n')}\n}`;
 };
